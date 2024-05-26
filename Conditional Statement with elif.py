feb6f25ec2679dedc8426b784d514392508c00e0
KusheelSanduri <sandurikusheel@gmail.com>
2024-05-26 16:17:15 +0530

    Create Conditional Statement with elif.py

---
+++ Conditional Statement with elif.py
@@ -0,0 +1,10 @@
+# Conditional Statement with elif
+ score = 91
+ if score >= 90:
+ print("Grade: A")
+ elif score >= 80:
+ print("Grade: B")
+ elif score >= 70:
+ print("Grade: C")
+ else:
+ print("Grade: F")
