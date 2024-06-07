291370d885509126c1ee01a7b6dc21a11856b4c4
KusheelSanduri <sandurikusheel@gmail.com>
2024-06-07 19:13:26 +0530

    Delete dictionary.py

--- dictionary.py
+++
@@ -1,6 +0,0 @@
-#dictionary
- my_dict = {'name': 'jay', 'Age': 21, 'city' : 'Hyderabad'}
- my_dict['gender'] = 'male'
- del my_dict['city']
- my_dict['age'] = 23
- print("Updated dictionary: ", my_dict)
